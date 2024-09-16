bd616bf7b762c046139d0d3f00088750deb7c1a8
Ayanaanonaeem <ayanaano5@gmail.com>
2024-09-17 00:27:12 +0500

    file create

---
+++ app.js
@@ -0,0 +1,107 @@
+var btn =document.getElementById("btn")
+var main= document.getElementById("main")
+console.log(main);
+
+btn.addEventListener("mouseenter", function () {
+   
+    main.style.justifyContent="center"
+    
+    btn.addEventListener("mouseenter", function () {
+   
+        main.style.justifyContent="flex-end"
+        
+        btn.addEventListener("mouseenter", function () {
+   
+            main.style.alignItems="center"
+            
+            btn.addEventListener("mouseenter", function () {
+   
+                main.style.alignItems="center"
+                main.style.justifyContent="center"
+                
+
+                btn.addEventListener("mouseenter", function () {
+   
+                    main.style.alignItems="center"
+                    main.style.justifyContent="flex-start"
+ 
+                    btn.addEventListener("mouseenter", function () {
+   
+                        main.style.alignItems=""
+                        main.style.justifyContent="flex-start"
+    
+                        btn.addEventListener("mouseenter", function () {
+   
+                        main.style.alignItems=""
+                        main.style.justifyContent="center"
+    
+                        btn.addEventListener("mouseenter", function () {
+   
+                            main.style.alignItems=""
+                            main.style.justifyContent="flex-end"
+
+                            btn.addEventListener("mouseenter", function () {
+   
+                                main.style.alignItems=""
+                                main.style.justifyContent="flex-start"
+
+                                btn.addEventListener("mouseenter", function () {
+   
+                                    main.style.flexDirection="row"
+                                    main.style.justifyContent="flex-start"
+
+                                    btn.addEventListener("mouseenter", function () {
+   
+                                        main.style.flexDirection="row"
+                                        main.style.justifyContent="flex-end"
+
+                                        btn.addEventListener("mouseenter", function () {
+   
+                                            main.style.flexDirection="row"
+                                            main.style.justifyContent="flex-start"
+
+                                            btn.addEventListener("mouseenter", function () {
+   
+                                                main.style.flexDirection="row"
+                                                main.style.justifyContent="flex-end"
+
+                                                btn.addEventListener("mouseenter", function () {
+   
+                                                    main.style.flexDirection="row"
+                                                    main.style.justifyContent="flex-start"
+
+
+                                                    btn.addEventListener("mouseenter", function () {
+   
+                                                        main.style.flexDirection="row"
+                                                        main.style.justifyContent="flex-end"
+                            
+                                                    })
+                        
+                                                })
+                    
+                                            })
+                
+                                        })
+            
+                                    })
+        
+                                })
+    
+                            })
+
+                        })
+                         
+                    })
+                         
+                    })
+                     
+                })
+            })
+           
+        })
+       
+    })
+    
+   
+})
